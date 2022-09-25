1811823c5c3436a1af0d4c4e4dcd52fd4d44ff55
mateuscosta27 <mateuscosta27@gmail.com>
2022-09-25 18:32:43 -0400

    creating project classes

--- model/Model.py
+++ model/Model.py
@@ -114,7 +114,7 @@ class Betano:
         self.df_full.to_csv(self.directory_file+'\\df_betano.csv',encoding='utf-8', sep=';', index=False)
         
         
-    def creating_index(self):
+    def creating_index_betano(self):
         
         ###criando indice no data frame devido aos nomes dos times estarem escritos diferentes nos sites###
         betano = pd.read_csv(self.directory_file+'\\df_betano.csv', encoding = 'utf-8', sep=';')
@@ -147,4 +147,96 @@ class Betano:
 
 
 
-class Pixbet
+class Pixbet:
+    
+    def __init__(self):
+        
+        
+        self.directory_file = 'C:\\tmp\\Arquivos'   ##Diretorio onde serão salvos os arquivos CSV com informações obtidas###     
+        self.directory_driver = 'C:\\tmp\\Driver'   ##Diretorio onde esta localizado o driver do google chrome para operação com selenium###    
+        self.path = self.directory_driver +'\\chromedriver.exe'
+        self.web_site = 'https://pixbet.com/prejogo/#league/2417-undefined' ##Site onde estamos buscando as informações###
+        self.options = webdriver.ChromeOptions()
+        self.options.add_experimental_option('excludeSwitches',['enable-logging'])
+        self.driver = webdriver.Chrome(options=self.options,executable_path=self.path)
+
+
+    def open_web_site(self):
+        self.driver.get(self.web_site)
+        self.driver.maximize_window()
+
+    def parser_data(self):
+        self.soup = BeautifulSoup(self.driver.page_source, 'lxml')
+        self.partidas =self.soup.find_all('table',{'class':'odds_table'})
+        self.driver.close()
+          
+    def data_transform(self):
+        self.dic_jogos = {'Jogos':[]}
+        for sub in self.soup.find_all('tr', {'class': 'odds_tr'}):
+            jogos = sub.get_text().strip().replace('\n\n\n\n',';').replace('\n\n\n',';').replace('\n\n',';')
+            self.dic_jogos['Jogos'].append(jogos) 
+            
+    def export_data(self):
+        self.directory_file = 'C:\\ApostasEsportivas'
+        if not os.path.exists(self.directory_file):
+            os.makedirs(self.directory_file)
+        df = pd.DataFrame(self.dic_jogos)
+        df.to_csv(self.directory_file+'\\scraping_pixbet.csv',encoding='utf-8', sep=';', index=False)
+        
+    def split_columns(self):
+        self.df = pd.read_csv(self.directory_file+'\\scraping_pixbet.csv',encoding='utf-8', sep=';')
+        self.df_split = self.df['Jogos'].str.split(';',expand= True)
+        self.df_split[['Data','Hora']] = self.df_split[0].str.split('|',expand=True)
+        self.df_split[['TimeCasa','TimeVisitante']] = self.df_split[1].str.split(' - ',expand=True)
+        self.df_split = self.df_split.drop(columns=[0,1,5,6,7,9,10,11,12,13,14,16,17,18,19,20,21,27])
+        
+        
+    def rename_columns(self):
+        self.df_split.columns=['Odds1','OddsX','Odds2','MaisGols',
+               'MenosGols','Dupla1x','Dupla12','Dupla2x','AmbosMarcamSim',
+               'AmbosMarcamNao','Data','Hora','TimeCasa','TimeVisitante']
+        
+    def order_columns(self):
+        self.df = pd.DataFrame(data=self.df_split)
+        self.df = self.df[['Hora','Data','TimeCasa','TimeVisitante','Odds1','OddsX','Odds2',
+                           'MaisGols','MenosGols','AmbosMarcamSim','AmbosMarcamNao','Dupla1x','Dupla12','Dupla2x']]
+        
+        
+    def remove_space(self):
+        for column_remove in self.df.columns:
+            self.df[column_remove] = self.df[column_remove].apply(lambda x:str(x).strip())
+    
+    def data_convert_types(self):
+        self.df['Data'] = pd.to_datetime(self.df['Data'],format="%d/%m")
+        for column_float in self.df.columns[4:]:
+           self.df[column_float] = pd.Series(self.df[column_float], dtype=float)
+        
+    def export_dataframe(self):
+        self.df.to_csv(self.directory_file+'\\DataFrame_pixbet.csv',encoding='utf-8', sep=';', index=False)
+        
+    def create_index_pixbet(self):
+        indice_pixbet={'Timec':[], 'Timev':[]}
+        df1 = self.pixbet['TimeCasa']
+        for nome in df1:
+            abrev = nome[0:3]
+            indice_pixbet['Timec'].append(abrev)
+                
+        df1 = self.pixbet['TimeVisitante']
+        for nome in df1:
+            abrev = nome[0:3]  
+            indice_pixbet['Timev'].append(abrev)
+                
+        df_pixbet = pd.DataFrame(indice_pixbet,columns=['Timec', 'Timev'])
+        df_pixbet['Index'] = df_pixbet['Timec'].map(str)+'x'+df_pixbet['Timev']
+
+        df_pixbet = df_pixbet.drop(['Timec', 'Timev'], axis=1)
+        self.pixbet.insert(0,'index',df_pixbet)
+        self.df_pixbet = df_pixbet
+        
+    def remove_file(self):
+        try:
+            os.remove(self.directory_file+'\\scraping_pixbet.csv')
+        except OSError as e:
+            print(f"Error:{e.strerror}")    
+        print(f"Arquivo esta no seguinte diretorio{self.directory_file}")
+    
