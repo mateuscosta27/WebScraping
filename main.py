82f0a2459dd32f0befc17461f28377459d63a611
mateuscosta27 <mateuscosta27@gmail.com>
2022-10-15 14:20:20 -0400

    bug fix

--- main.py
+++ main.py
@@ -287,29 +287,15 @@ class ModuloPrincipal(QMainWindow):
         """Renomeia as colunas de acordo com a seleção
            Renaming the columns according to the selection
         """
+        names = ['Data','Campeonato','Mandante','Visitante','SPI1','SPI2','VitoriaMandante','Empate',
+        'VitoriaVisitante','GolsMandante', 'Golsvisitante']
+
         try:
-            item = self.ui.tb_preview.horizontalHeaderItem(0)
-            item.setText('Data')
-            item = self.ui.tb_preview.horizontalHeaderItem(1)
-            item.setText('Campeonato')
-            item = self.ui.tb_preview.horizontalHeaderItem(2)
-            item.setText('Mandante')
-            item = self.ui.tb_preview.horizontalHeaderItem(3)
-            item.setText('Visitante')
-            item = self.ui.tb_preview.horizontalHeaderItem(4)
-            item.setText('spi1')
-            item = self.ui.tb_preview.horizontalHeaderItem(5)
-            item.setText('spi2')
-            item = self.ui.tb_preview.horizontalHeaderItem(6)
-            item.setText('VitoriaMandante')
-            item = self.ui.tb_preview.horizontalHeaderItem(7)
-            item.setText('Empate')
-            item = self.ui.tb_preview.horizontalHeaderItem(8)
-            item.setText('VitoriaVisitante')
-            item = self.ui.tb_preview.horizontalHeaderItem(9)
-            item.setText('GolsMandante')
-            item = self.ui.tb_preview.horizontalHeaderItem(10)
-            item.setText('GolsVisitante')
+             
+            for i in range(11):
+                item = self.ui.tb_preview.horizontalHeaderItem(i)
+                item.setText(names(lambda x: x in names))    
+            
         except Exception as e:
             print(f'Algo não esta correto:{e} ')
             pass
@@ -324,7 +310,6 @@ class ModuloPrincipal(QMainWindow):
         """Lista as probabilidades boas para apostas
            List good odds for betting
         """
-        
         directory_database = 'C:\\tmp\\Bancos'
         con_db = sqlite3.connect(directory_database+'\\DADOS.db')
         mycursor = con_db.cursor()
@@ -342,20 +327,18 @@ class ModuloPrincipal(QMainWindow):
                                     when b.Odds2 = 'None' then 'Sem dados'
                                     ELSE "Sem Oportunidades"
                                     END as Aportunidade2
-                                    
-                                    
                                             from tb_pixbet p
                                                 inner join tb_betano b
                                                 on instr(p.ind ,b.ind)>0;""")
-
         resultSet = mycursor.fetchall()
         self.ui.tb_view.setRowCount(len(resultSet))
         self.ui.tb_view.setColumnCount(10)
-        self.double_odds_name()  # função para renomear as colunas
+        self.match_odds_name()  # função para renomear as colunas
         for i in range(0, len(resultSet)):
             for j in range(0, 10):
                 self.ui.tb_view.setItem(i, j, QtWidgets.QTableWidgetItem(str(resultSet[i][j])))
         con_db.close()
+            
     def list_match_odds(self):
         """Lista as probabilidades boas para apostas
            List good odds for betting
---
+++ tests/teste_names.py
@@ -0,0 +1,9 @@
+names = ['Data','Campeonato','Mandante','Visitante','SPI1','SPI2','VitoriaMandante','Empate',
+        'VitoriaVisitante','GolsMandante', 'Golsvisitante']             
+
+
+item =lambda x: x in names
+
+
+
+print(item)
